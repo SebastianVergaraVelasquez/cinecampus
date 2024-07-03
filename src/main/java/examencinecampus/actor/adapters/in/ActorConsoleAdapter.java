4b99d712072b0592a10fa01f6374c6133d3dacdd
SebastianVergaraVelasquez <sv.velasquez36@gmail.com>
2024-07-03 07:24:02 -0500

    feat: :construction: Actor console test

--- src/main/java/examencinecampus/actor/adapters/in/ActorConsoleAdapter.java
+++ src/main/java/examencinecampus/actor/adapters/in/ActorConsoleAdapter.java
@@ -1,5 +1,12 @@
 package examencinecampus.actor.adapters.in;
 
+import examencinecampus.actor.application.ActorService;
+
 public class ActorConsoleAdapter {
-    
+    private final ActorService actorService;
+
+    public ActorConsoleAdapter(ActorService actorService) {
+        this.actorService = actorService;
+    }
+
 }
